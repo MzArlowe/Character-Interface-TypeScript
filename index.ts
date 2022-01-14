56876d57b690e4648853bb37553bbbc11897e2cc
unknown <bhm727@yahoo.com>
2022-01-14 11:18:29 -0500

    First push to my Repo

--- index.ts
+++ index.ts
@@ -1,26 +1,161 @@
-//
-//            Create an interface called:
-//?                   CharacterInterface
-//
-//            Give this interface the following variable names and type information:
-//?                    name: string;
-//?                    health: number;
-//?                    attack: number;
-//?                    defense: number;
-//?                    accuracy: number;
-//
-//            Create a Character class, assign it the interface above. This class will have a constructor that will accept and declare the variables in the interface above.
-//
-//            Create two classes, Player and Enemy, each one needs to inherit properties and methods from the Character class. These classes will also use constructors.
-//
-//            Within the Character class create a method called:
-//?             attackTarget
-//              ..that accepts a parameter called:
-//?               target
-//                This parameter will represent an instantiated class of Player or Enemy. Provide this parameter accurate type information, and include this method to the CharacterInterface.
-//
-//              This function should log the name of the character that is attacking, as well as the name of the target.
-//
-//            Create a new instance of the Player class and the Enemy class passing along the correct arguments for (name..health..attack..etc).
-//
-//            Call the attack function on the new Player instance, and pass in the new Enemy instance, see if it functions correctly.
+//Created Character Classes using Interface and designing a randomized attack system using TypeScript
+
+interface CharacterInterface {
+    name: string;
+    health: number;
+    maxHealth: number,
+    attack: number;
+    defense: number;
+    accuracy: number;
+    healthBar: HealthIcon[];
+    attackTarget(target: CharacterInterface): void;
+    showHealth: () => void;
+}
+
+type HealthIcon = "🟩" | "🟥";
+class Character implements CharacterInterface {
+    healthBar: HealthIcon[] = [
+        "🟩",
+        "🟩",
+        "🟩",
+        "🟩",
+        "🟩",
+        "🟩",
+        "🟩",
+        "🟩",
+        "🟩",
+        "🟩",
+    ];
+
+    maxHealth: number = this.health;
+    constructor(
+        public name: string,
+        public health: number,
+        public attack: number,
+        public defense: number,
+        public accuracy: number
+    ) { }
+
+    attackTarget(target: CharacterInterface) {
+        //Check Attacker Health
+        if (this.health <= 0) {
+            return;
+        } else {
+            //Check Target Health
+            if (target.health > 0) {
+                //Attack Landed
+                if (Math.random() > 1 - this.accuracy) {
+                    let damage: number = 
+                    this.attack * (100 / 100 + target.defense) + Math.floor(Math.random() * 5);
+                    console.log(
+                        `${this.name} is attacking ${target.name} with ${damage} damage!💥 \n`
+                        );
+                    target.health -= damage;
+
+                    // Checking Targets Health Percent
+                    let targetHealthPercent = (target.health / target.maxHealth).toFixed(2);
+                    if (targetHealthPercent.toString()[0] === "-") {
+                        for (let x = 0; x < target.healthBar.length; x++) {
+                            target.healthBar[x] = "🟥";
+                        }
+                    } else {
+                        target.healthBar = [
+                            "🟥",
+                            "🟥",
+                            "🟥",
+                            "🟥",
+                            "🟥",
+                            "🟥",
+                            "🟥",
+                            "🟥",
+                            "🟥",
+                            "🟥",
+                        ];
+                        for (let y = 0; y < +targetHealthPercent.toString()[2]; y++) {
+                            target.healthBar[y] = '🟩';
+                        }
+                        target.showHealth();
+                    }
+                    //Check if target is still alive
+                    if (target.health <= 0) {
+                        console.log(`${target.name} was defeated by ${this.name} ... 💀\n`)
+                    }
+                } else {
+                    console.log(`${this.name}'s Attack Missed! \n`)
+                }
+            }
+        }
+    }
+
+    async rest() {
+        console.log("Resting");
+        return new Promise((resolve, reject): void => {
+            setTimeout(() => {
+                console.log("Rest Complete. HP Restored");
+                this.health = this.maxHealth;
+                resolve("");
+            }, 3000);
+        });
+    }
+
+    showHealth() {
+        console.log(`[${this.name}]`)
+        console.log(`${this.healthBar.join('')}`)
+        console.log(`HP: ${this.health}/ ${this.maxHealth} \n`);
+    }
+}
+
+class Player extends Character {
+    constructor(
+        public name: string,
+        public health: number,
+        public attack: number,
+        public defense: number,
+        public accuracy: number
+    ) {
+        super(name, health, attack, defense, accuracy);
+    }
+}
+
+class Enemy extends Character {
+    constructor(
+        public name: string,
+        public health: number,
+        public attack: number,
+        public defense: number,
+        public accuracy: number
+    ) {
+        super(name, health, attack, defense, accuracy);
+    }
+}
+
+const Player1: Player = new Player('Dragon Slayer', 3130, 50, 30, 0.5);
+const AI_Entry_Creature: Enemy = new Enemy("Fire-Rabbit", 500, 5, 7, 0.3)
+const AI_Ice_Dragon_Boss: Enemy = new Enemy(
+    "Ice Dragon",
+    5050,
+    25,
+    18,
+    0.4
+)
+
+const maxNumOfTurns = 100;
+const engageCombat = (a: CharacterInterface, b: CharacterInterface) => {
+    for (let i = 1; i < maxNumOfTurns; i++) {
+        if (Math.random() > .5) {
+            a.attackTarget(b)
+        } else {
+            b.attackTarget(a);
+        }
+    }
+}
+const ExecuteEvents = async () => {
+    //Fight first creature
+    engageCombat(Player1, AI_Entry_Creature);
+    //break and restore healthBar
+    await Player1.rest()
+    //Fight Big Bad Boss
+    engageCombat(Player1, AI_Ice_Dragon_Boss);
+};
+
+ExecuteEvents();
